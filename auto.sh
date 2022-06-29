d2d0b5c66aab5fa4b695f392e47686ff48febbe0
pedrolucasp1 <40402385+pedrolucasp1@users.noreply.github.com>
2022-06-29 17:49:36 -0300

    Add files via upload

--- auto.sh
+++ auto.sh
@@ -134,24 +134,22 @@ case $option in
         echo 'fi' >> /etc/bash.bashrc
         sed -i 's/"syntax on/syntax on/' /etc/vim/vimrc
         sed -i 's/"set background=dark/set background=dark/' /etc/vim/vimrc
-        cat <<EOF >/root/.vimrc
-        set showmatch " Mostrar colchetes correspondentes
-        set ts=4 " Ajuste tab
-        set sts=4 " Ajuste tab
-        set sw=4 " Ajuste tab
-        set autoindent " Ajuste tab
-        set smartindent " Ajuste tab
-        set smarttab " Ajuste tab
-        set expandtab " Ajuste tab
-        "set number " Mostra numero da linhas
-        EOF
+        sed -i "1,20d" /root/.vimrc
+        echo "set showmatch" >> /root/.vimrc
+        echo "set ts=4" >> /root/.vimrc
+        echo "set sts=4" >> /root/.vimrc
+        echo "set sw=4" >> /root/.vimrc
+        echo "set autoindent" >> /root/.vimrc
+        echo "set smartindent" >> /root/.vimrc
+        echo "set smarttab" >> /root/.vimrc
+        echo "set expandtab" >> /root/.vimrc
+        echo '"set number' >> /root/.vimrc
         sed -i "s/# export LS_OPTIONS='--color=auto'/export LS_OPTIONS='--color=auto'/" /root/.bashrc
         sed -i 's/# eval "`dircolors`"/eval "`dircolors`"/' /root/.bashrc
         sed -i 's/# eval "$(dircolors)"/eval "$(dircolors)"/' /root/.bashrc
         sed -i "s/# alias ls='ls \$LS_OPTIONS'/alias ls='ls \$LS_OPTIONS'/" /root/.bashrc
         sed -i "s/# alias ll='ls \$LS_OPTIONS -l'/alias ll='ls \$LS_OPTIONS -l'/" /root/.bashrc
         sed -i "s/# alias l='ls \$LS_OPTIONS -lA'/alias l='ls \$LS_OPTIONS -lha'/" /root/.bashrc
-        echo '# Para usar o fzf use: CTRL+R' >> ~/.bashrc
         echo 'source /usr/share/doc/fzf/examples/key-bindings.bash' >> ~/.bashrc
         echo "alias grep='grep --color'" >> /root/.bashrc
         echo "alias egrep='egrep --color'" >> /root/.bashrc
